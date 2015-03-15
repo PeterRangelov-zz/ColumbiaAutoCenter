83edfdc233562277b78aa2cf0f86f26625868665
Peter Rangelov <peter.rangelov11@gmail.com>
2015-03-15 13:59:06 -0400

    -

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,28 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render());
     }
+
+    public static Result ourTeam() {
+        return TODO;
+    }
+
+    public static Result aboutUs() {
+        return TODO;
+    }
+
+    public static Result contactUs() {
+        return TODO;
+    }
+
+    public static Result partsForSale() {
+        return TODO;
+    }
+
+    public static Result services() {
+        return TODO;
+    }
+
 
 }
