f3d1f050282c45e5b515d307aa721a235e73853c
Peter Rangelov <peter.rangelov11@gmail.com>
2015-03-19 21:04:36 -0400

    -

---
+++ app/Global.java
@@ -0,0 +1,20 @@
+import play.Application;
+import play.GlobalSettings;
+import play.Logger;
+import play.Play;
+
+public class Global extends GlobalSettings {
+
+    @Override
+    public void onStart(Application app) {
+        Logger.info("APPLICATION HAS STARTED");
+        Logger.info("SENDGRID USERNAME -> "+Play.application().configuration().getString("sendgrid.username"));
+        Logger.info("SENDGRID PASSWORD -> "+Play.application().configuration().getString("sendgrid.password"));
+    }
+
+    @Override
+    public void onStop(Application app) {
+        Logger.info("Application shutdown...");
+    }
+
+}
